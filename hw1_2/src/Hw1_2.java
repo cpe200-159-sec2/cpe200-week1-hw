55bc167252a98057f17c54772e62e01a29e7d20a
supakarn39 <supakarn39@hotmail.com>
2016-08-26 20:25:01 +0700

    Finish Homework 1-2

--- hw1_2/src/Hw1_2.java
+++ hw1_2/src/Hw1_2.java
@@ -4,10 +4,13 @@
 public class Hw1_2 {
     float gpaCalculation(int[] credit, int[] grade)
     {
-        /**
-         * your code here
-         */
-        return 0.0f;
+        float multiple=0;
+        int allCredit=0;
+        for(int i=0; i<credit.length; i++){
+            multiple += credit[i]*grade[i];
+            allCredit += credit[i];
+        }
+        return multiple/allCredit;
     }
 
     public static void main(String[] args) {
