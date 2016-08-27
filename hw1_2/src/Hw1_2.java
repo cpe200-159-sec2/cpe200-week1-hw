326861100044e38b9d57c153ab364f4ee9e7d418
GEST <naluedomgest@gmail.com>
2016-08-28 00:50:42 +0700

    complete

--- hw1_2/src/Hw1_2.java
+++ hw1_2/src/Hw1_2.java
@@ -3,11 +3,12 @@
  */
 public class Hw1_2 {
     float gpaCalculation(int[] credit, int[] grade)
-    {
-        /**
-         * your code here
-         */
-        return 0.0f;
+    {   float x=0,tcredit=0;
+        for(int i=0; i<credit.length;i++){
+            x += credit[i]*grade[i];
+            tcredit += credit[i];
+        }
+        return x/tcredit;
     }
 
     public static void main(String[] args) {
