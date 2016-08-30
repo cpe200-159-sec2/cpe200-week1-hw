7dbd59118fc8d80be625fe36a3510670580c1e73
Phumiphat_C <guitar11161@gmail.com>
2016-08-30 19:14:19 +0700

    all hw has done :)

--- hw1_2/src/Hw1_2.java
+++ hw1_2/src/Hw1_2.java
@@ -2,14 +2,14 @@
  * Created by pruet on 11/8/2559.
  */
 public class Hw1_2 {
-    float gpaCalculation(int[] credit, int[] grade)
-    {
-        /**
-         * your code here
-         */
-        return 0.0f;
+    float gpaCalculation(int[] credit, int[] grade) {
+        float sum = 0, sumc = 0;
+        for (int i = 0; i < credit.length; i++) {
+            sum += credit[i] * grade[i];
+            sumc += credit[i];
+        }
+        return sum/sumc;
     }
-
     public static void main(String[] args) {
         int[] credit = {3, 3, 3, 3, 2};
         int[] grade  = {2, 4, 4, 3, 2};
