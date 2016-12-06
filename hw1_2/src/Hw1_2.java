a4df85bee910243f31554bed71f4a6afc29d1700
ahar0007 <ahar0007@hotmail.co.th>
2016-12-07 00:37:12 +0700

    580610693

--- hw1_2/src/Hw1_2.java
+++ hw1_2/src/Hw1_2.java
@@ -4,10 +4,14 @@
 public class Hw1_2 {
     float gpaCalculation(int[] credit, int[] grade)
     {
-        /**
-         * your code here
-         */
-        return 0.0f;
+        float gpa = 0;
+        float divi = 0;
+        for (int i = 0; i < credit.length; i++) {
+            gpa += (credit[i] * grade[i]);
+            divi += (credit[i]);
+        }
+
+        return gpa / divi;
     }
 
     public static void main(String[] args) {
